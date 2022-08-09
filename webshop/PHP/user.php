7e611323ab499b473d56b7e1f84312fb6925b2f2
Simon <roehrs1@uni-bremen.de>
2022-08-09 18:49:17 +0200

    aktueller Stand Dienstag

--- webshop/PHP/user.php
+++ webshop/PHP/user.php
@@ -1,12 +1,18 @@
 <?php
 include('setupDB.php');
 
-
+session_start();
+// Holen: Name, Vorname, Email, Passwort
 $name = $_POST['name'];         // [...] müsste aus formular2.html Inhalt nehmen
 $vorname = $_POST['vorname'];
 $email = $_POST['email'];
 $password = $_POST['passwort'];
 
+// Speichern: Name, Vorname, Email und Passwort als Session-Daten
+$_SESSION['name'] = $name;
+$_SESSION['vorname'] = $vorname;
+$_SESSION['email'] = $email;
+$_SESSION['password'] = $password;
 
 //preparation of insert statement
 $tname = 'nutzer';
@@ -17,6 +23,8 @@ $name4= 'password';
 $ins = "INSERT INTO $tname($name1, $name2, $name3, $name4) VALUES (?, ?, ?, ?)";
 $ps = $conn -> prepare($ins);
 
+// Für Übertragung des Namens zum Warenkorb (nicht fertig)
+echo "Hallo, $vorname $name! Das hier ist dein Warenkorb.<br/>";
 
 //function to insert a new user
 function insertUser($p, $n1, $n2, $n3, $n4) {
