8e54c0a63ce82e4724489e67dd76fc09a9e34a35
Hannes Wattenberg <hannes.m.wattenberg@web.de>
2022-08-14 12:01:31 +0200

    Implemented AJAX for addArticle

--- webshop/PHP/addArticle.php
+++ webshop/PHP/addArticle.php
@@ -9,32 +9,90 @@
                     <div class="grid-title">
                         <h2>Artikel hinzufügen</h2>
                     </div>
-                        <form action="article.php" method="post" enctype="multipart/form-data">
+                    <h2 style='text-align: center' id="response"></h2><br>
+                        <!-- <form action="article.php" method="post" enctype="multipart/form-data"> -->
                             <table>
                                 <tr>
                                     <td><label for="produktname">Name</label></td>
-                                    <td><input type="text" name="produktname" id="produktname"></td>
+                                    <td><input type="text" style="width:600px;" name="produktname" id="produktname"></td>
                                 </tr>
                                 <tr>
                                     <td><label for="preis">Preis</label></td>
-                                    <td><input type="text" name="preis" id="preis"></td>
+                                    <td><input type="text" style="width:600px;" name="preis" id="preis"></td>
                                 </tr>
                                 <tr>
                                     <td><label for="anzahl">Anzahl</label></td>
-                                    <td><input type="text" name="anzahl" id="anzahl"></td>
+                                    <td><input type="number" style="width:600px;" name="anzahl" id="anzahl"></td>
                                 </tr>
                                 <tr>
                                     <td><label for="foto">Foto</label></td>
-                                    <td><input type="file" name="foto" id="foto"></td>
+                                    <td><input type="file" style="width:600px;" name="foto" id="foto"></td>
                                 <tr>
                                     <td><label for="beschreibung">Beschreibung</label></td>
                                     <td><textarea name="beschreibung" id="beschreibung" class="description"></textarea></td>
                             </tr>
                             </table>
                             <br>
-                            <button type="submit" value="Artikel hinzufügen">Artikel hinzufügen</button>
+                            <button type="submit" id="addArticle" value="Artikel hinzufügen">Artikel hinzufügen</button>
                             <br>
-                        </form>
+                        <!-- </form> -->
+                        <script>
+                            window.addEventListener("load", init);
+
+                            function init() {
+                                document.getElementById("addArticle").addEventListener("click", ajax);
+                                document.getElementById("addArticle").addEventListener("click", clearInput);
+                            }
+
+                            function clearInput(){
+
+                              var inputs = document.querySelectorAll('#produktname, #preis, #anzahl, #foto, #beschreibung');
+
+                              inputs.forEach(input => {
+                                input.value = '';
+                              });
+                            }
+
+
+                            function ajax() {
+
+                                var produktname = document.getElementById("produktname").value;
+                                var preis = document.getElementById("preis").value;
+                                var anzahl = document.getElementById("anzahl").value;
+                                var fotoInput = document.getElementById("foto");
+                                var foto = fotoInput.files[0];
+                                var beschreibung = document.getElementById("beschreibung").value;
+
+                                if (produktname && preis && anzahl && foto && beschreibung) {
+
+                                    var ajaxRequest = new XMLHttpRequest();
+                                    ajaxRequest.addEventListener("load", ajaxGeladen);
+                                    ajaxRequest.addEventListener("error", ajaxFehler);
+                                    ajaxRequest.open("post", "article.php");
+                                    var daten = new FormData();
+                                    daten.append("produktname", produktname);
+                                    daten.append("preis", preis);
+                                    daten.append("anzahl", anzahl);
+                                    daten.append("foto", foto, foto.name);
+                                    daten.append("beschreibung", beschreibung);
+                                    ajaxRequest.send(daten);
+                                }
+                                else {
+                                    spricht("Bitte Fülle alle Felder aus");
+                                }
+                            }
+                            function ajaxGeladen(event) {
+                                // var antwort = JSON.parse(event.target.responseText);
+                                var antwort = JSON.parse(this.responseText);
+                                spricht(antwort.nachricht);
+                            }
+                            function spricht(text) {
+                                document.getElementById("response").innerHTML = text;
+                            }
+                            function ajaxFehler(event) {
+                                alert(event.target.statusText);
+                            }
+                        </script>
             </div>
         </div>
 
--- webshop/PHP/article.php
+++ webshop/PHP/article.php
@@ -1,40 +1,32 @@
 <?php
-
 include('setupDB.php');
+require_once ('functions.inc.php');
 
-$name = $_POST['produktname'];     // [...] müsste aus formular3.html Inhalt nehmen
-$preis = $_POST['preis'];
-$amount = $_POST['anzahl'];
-$description = $_POST['beschreibung'];
-$bildPath = $_FILES['foto']['tmp_name'];    // der Pfad der Datei, die hochgeladen wird
+if (isset($_REQUEST["produktname"]) && isset($_REQUEST["preis"]) && isset($_REQUEST["anzahl"]) && isset($_FILES['foto']['tmp_name']) && isset($_REQUEST["beschreibung"]) ) {
 
-//preparation of insert statement
-$tname = 'artikel';
-$name1 = 'name';
-$name2 = 'preis';
-$name3 = 'anzahl';
-$name4 = 'beschreibung';
-$name5 = 'bild';
+  $name = $_REQUEST["produktname"];     // [...] müsste aus formular3.html Inhalt nehmen
+  $preis = $_REQUEST["preis"];
+  $amount = $_REQUEST["anzahl"];
+  $description = $_REQUEST["beschreibung"];
+  $bildPath = $_FILES['foto']['tmp_name'];    // der Pfad der Datei, die hochgeladen wird
 
-//function to insert a new user
-function insertArticle($p, $n1, $n2, $n3, $n4, $b) {
-    $blob = file_get_contents($b); // der Inhalt des Bildes mit dem Path $b
-    $sql = "INSERT INTO artikel(name, preis, anzahl, beschreibung, bild) VALUES('$n1', '$n2', '$n3', '$n4', 0x".bin2hex($blob).")";
 
-    if (!mysqli_query($p, $sql)) {
-        die("Hinzufügen des Bildes ist fehlgeschlagen: " . mysqli_error());
+  //function to insert a new user
+  function insertArticle($p, $n1, $n2, $n3, $n4, $b) {
+      $blob = file_get_contents($b); // der Inhalt des Bildes mit dem Path $b
+      $sql = "INSERT INTO artikel(name, preis, anzahl, beschreibung, bild) VALUES('$n1', '$n2', '$n3', '$n4', 0x".bin2hex($blob).")";
+
+      if (!mysqli_query($p, $sql)) {
+          die("Hinzufügen des Bildes ist fehlgeschlagen: " . mysqli_error());
+      }
+      echo '{"nachricht": "Jo Klappt"}';
     }
-    echo ($n1 . " wurde erfolgreich hinzugefügt! <br>");
-  }
 
-$f1 = $name;
-$f2 = $preis;
-$f3 = $amount;
-$f4 = $description;
-$f5 = $bildPath;
 
-//execution of the insertion function
-insertArticle($conn, $f1, $f2, $f3, $f4, $f5);
+  //execution of the insertion function
+  insertArticle($conn, $name, $preis,$amount, $description, $bildPath);
+
+}
 
 // close the connection
 $conn -> close();
